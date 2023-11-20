1388efd0463ee56d59b1d1b88411aef7d85c7a01
IgorSteps <igorstepanenko696@gmail.com>
2023-11-20 17:25:14 +0000

    Adding a sphere

--- include/renderer/renderer.h
+++ include/renderer/renderer.h
@@ -6,6 +6,8 @@ class Renderer {
 public:
 	Renderer(Image& img, Camera& cam): m_Img(img), m_Camera(cam) {}
 	void Render() ;
+
+	bool HitSphere(const glm::vec3& center, double radius, const Ray& r);
 	glm::vec3 CalculateRayColour(const Ray& r);
 private:
 	Camera m_Camera;
