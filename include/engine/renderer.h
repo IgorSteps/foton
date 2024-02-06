a1687b951152110e32fea02045949fccfacfec74
IgorSteps <igorstepanenko696@gmail.com>
2024-02-06 15:16:30 +0000

    sp

--- include/engine/renderer.h
+++ include/engine/renderer.h
@@ -1,16 +1,16 @@
-#pragma once
-#include <core/image.h>
-#include <core/camera.h>
-#include <geometry/sphere.h>
-
-class Renderer {
-public:
-	Renderer(Image& img, Camera& cam, Sphere& sphere): m_Img(img), m_Camera(cam), m_Sphere(sphere) {}
-	void Render() ;
-
-	glm::vec3 CalculateRayColour(const Ray& r);
-private:
-	Camera m_Camera;
-	Image m_Img;
-	Sphere m_Sphere;
-};
+//#pragma once
+//#include <core/image.h>
+//#include <core/camera.h>
+//#include <geometry/sphere.h>
+//
+//class Renderer {
+//public:
+//	Renderer(Image& img, Camera& cam, Sphere& sphere): m_Img(img), m_Camera(cam), m_Sphere(sphere) {}
+//	void Render() ;
+//
+//	glm::vec3 CalculateRayColour(const Ray& r);
+//private:
+//	Camera m_Camera;
+//	Image m_Img;
+//	Sphere m_Sphere;
+//};
