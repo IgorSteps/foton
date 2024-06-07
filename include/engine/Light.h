532be78616fa34804aa503cd877acd71fbdbf16b
Igor Stepanenko <84919282+IgorSteps@users.noreply.github.com>
2024-06-07 13:05:29 +0100

    Some more cleanup for the demo and readme update (#25)

--- include/engine/Light.h
+++ include/engine/Light.h
@@ -5,11 +5,23 @@
 class Light {
 public:
     glm::vec3 position;
-    glm::vec3 color;
+    glm::vec3 colour;
     float intensity;
     float elapsedTime = 0.0f;
 
     Light(const glm::vec3& pos, const glm::vec3& col, float intens)
-        : position(pos), color(col), intensity(intens) {}
+        : position(pos), colour(col), intensity(intens) {}
     Light() {};
+
+    void Update(float dt)
+    {
+        elapsedTime += dt;
+        // Move the light in a circle areound x and z axis.
+        float radius = 2.0f; 
+        float speed = 0.3f; 
+        float lightX = cos(elapsedTime * speed) * radius;
+        float lightZ = sin(elapsedTime * speed) * radius;
+
+        position = glm::vec3(lightX, position.y, lightZ);
+    }
 };
